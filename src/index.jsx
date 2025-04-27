33f08442120c9826c9bfd9d3a44f4cc574edb3ab
Dandi <dandi.juliandi@nawadata.id>
2025-04-27 09:56:22 +0700

    add roboto font

--- src/index.jsx
+++ src/index.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 
-import routes from './routes';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 import Layout from '@/page/layout';
+
+import routes from './routes';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
