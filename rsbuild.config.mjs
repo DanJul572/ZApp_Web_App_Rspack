83ee5c823a58c1a5a7d4895da02eca6aeb34ab10
Dandi <juliandidandi4@gmail.com>
2025-08-17 18:44:22 +0700

    change dist folder

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,21 +1,8 @@
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import Dotenv from 'dotenv-webpack';
-import path from 'path';
 
 export default defineConfig({
-  build: {
-    lib: [
-      {
-        entry: {
-          index: path.resolve(__dirname, 'src/index.jsx'), // entry utama
-          button: path.resolve(__dirname, 'src/components/button/Group.jsx'),
-        },
-        format: ['esm', 'cjs'],
-      },
-    ],
-    outDir: 'dist',
-  },
   plugins: [pluginReact()],
   html: {
     title: process.env.APP_NAME,
