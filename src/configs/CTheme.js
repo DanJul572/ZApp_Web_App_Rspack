a3327c09ce4a64744b27202e1799d0a57819a4b3
Dandi <juliandidandi4@gmail.com>
2025-09-13 11:42:24 +0700

    styling topbar

--- src/configs/CTheme.js
+++ src/configs/CTheme.js
@@ -4,9 +4,9 @@ const theme = createTheme({
   palette: {
     mode: process.env.THEME || 'light',
     primary: {
-      light: '#90a4ae',
-      main: '#607d8b',
-      dark: '#455a64',
+      light: '#ffec8b', // amber soft
+      main: '#ffb400', // orange agak merah (utama)
+      dark: '#c77800', // coklat–orange gelap
     },
     secondary: {
       light: '#D2E8D4',
--- src/layouts/Topbar.jsx
+++ src/layouts/Topbar.jsx
@@ -1,14 +1,17 @@
 import CTheme from '@configs/CTheme';
 import Logout from '@mui/icons-material/Logout';
+import { useTheme } from '@mui/material';
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import { useNavigate } from 'react-router';
 import { useExpandedMenu } from '@/contexts/ExpandedMenuProvider';
 import auth from '@/helpers/auth';
+import { version } from '../../package.json';
 
 const Topbar = () => {
   const navigate = useNavigate();
+  const theme = useTheme();
   const { setExpandedMenu } = useExpandedMenu();
 
   const logout = () => {
@@ -27,10 +30,40 @@ const Topbar = () => {
         backgroundColor: CTheme.palette.primary.main,
       }}
     >
-      <Typography variant="h6" noWrap component="div" color="inherit">
-        {process.env.APP_NAME || 'ZApp'}
-      </Typography>
-      <IconButton onClick={logout} color="inherit">
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'flex-end',
+          justifyContent: 'center',
+          gap: 0.5,
+        }}
+      >
+        <Typography
+          variant="h6"
+          noWrap
+          component="div"
+          sx={{ fontWeight: 'bold', color: theme.palette.primary.contrastText }}
+        >
+          {process.env.APP_NAME || 'ZApp'}
+        </Typography>
+        <Typography
+          riant="subtitle2"
+          component="span"
+          sx={{ fontWeight: 500, color: theme.palette.primary.contrastText }}
+        >
+          {`v${version}`}
+        </Typography>
+      </Box>
+      <IconButton
+        onClick={logout}
+        sx={{
+          bgcolor: 'primary.main',
+          color: theme.palette.primary.contrastText,
+          '&:hover': {
+            bgcolor: 'primary.dark',
+          },
+        }}
+      >
         <Logout />
       </IconButton>
     </Box>
