c0dd1a2c955f8841ace6dfb8a5ffa324c8c1dc54
DanJul572 <juliandidandi4@gmail.com>
2026-04-26 19:53:56 +0700

    update library

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -61,14 +61,16 @@ const Page = () => {
 
   return (
     <Box
-      alignItems="center"
-      bottom={0}
-      display="flex"
-      justifyContent="center"
-      left={0}
-      position="absolute"
-      right={0}
-      top={0}
+      sx={{
+        alignItems: 'center',
+        bottom: 0,
+        display: 'flex',
+        justifyContent: 'center',
+        left: 0,
+        position: 'absolute',
+        right: 0,
+        top: 0,
+      }}
     >
       <Card
         sx={{
